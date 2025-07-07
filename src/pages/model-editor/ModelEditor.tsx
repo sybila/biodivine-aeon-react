9b72ab7026cc0fe656cdfa4b41969b2785f4b8b7
MrMooshaber <vojtavesely1@seznam.cz>
2025-07-07 15:58:57 +0200

    feat: Model Editor side button menu added

--- src/pages/model-editor/ModelEditor.tsx
+++ src/pages/model-editor/ModelEditor.tsx
@@ -1,13 +1,47 @@
 import SideButtonMenu from "../../components/react-components/global/SideButtonMenu/SideButtonMenu";
 import ModelEditorCanvas from "../../components/react-components/model-editor/ModelEditorCanvas/ModelEditorCanvas";
 import IconButtonReact from "../../components/react-components/lit-wrappers/IconButtonReact";
-import PlayIcon from "../../assets/icons/play_circle_filled-48px.svg"
+
+import PlayIcon from "../../assets/icons/play_circle_filled-48px.svg";
+import FileIcon from "../../assets/icons/file_copy-48px.svg";
+import ModelIcon from "../../assets/icons/model-48px.svg";
+import ControlIcon from "../../assets/icons/control-enabled-48px.svg";
+import PhenotypeIcon from "../../assets/icons/phenotype-48px.svg";
 
 const ModelEditor: React.FC = () => {
   return (
     <>
       <SideButtonMenu>
-        <IconButtonReact compHeight="60px" compWidth="100%" buttonSize="100%" iconSrc={PlayIcon} iconAlt="Play" showTag={true} tagWidth="100%" tagText="Start Computation" ></IconButtonReact>
+        <IconButtonReact
+          iconSrc={PlayIcon}
+          iconAlt="Play"
+          showTag={true}
+          tagText="Start Computation"
+        ></IconButtonReact>
+        <IconButtonReact
+          iconSrc={FileIcon}
+          iconAlt="File"
+          showTag={true}
+          tagText="Import/Export"
+        ></IconButtonReact>
+        <IconButtonReact
+          iconSrc={ModelIcon}
+          iconAlt="Model"
+          showTag={true}
+          tagText="Model Editor"
+        ></IconButtonReact>
+        <IconButtonReact
+          iconSrc={ControlIcon}
+          iconAlt="Control"
+          showTag={true}
+          tagText="Control-Enabled Editor"
+        ></IconButtonReact>
+        <IconButtonReact
+          iconSrc={PhenotypeIcon}
+          iconAlt="Phenotype"
+          showTag={true}
+          tagText="Phenotype Editor"
+        ></IconButtonReact>
       </SideButtonMenu>
       <ModelEditorCanvas></ModelEditorCanvas>
     </>
