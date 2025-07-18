099305c912d3fd4cfa448976bea976711a1ed809
MrMooshaber <vojtavesely1@seznam.cz>
2025-07-18 14:25:47 +0200

    chore: ModelEditor.tsx formated

--- src/pages/model-editor/ModelEditor.tsx
+++ src/pages/model-editor/ModelEditor.tsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
-import SideButtonMenu from "../../components/react-components/global/SideButtonMenu/SideButtonMenu";
-import ModelEditorCanvas from "../../components/react-components/model-editor/ModelEditorCanvas/ModelEditorCanvas";
-import IconButtonReact from "../../components/react-components/lit-wrappers/IconButtonReact";
-import PopUpBarReact from "../../components/react-components/lit-wrappers/PopUpBarReact";
-import NavigationDockContent from "../../components/react-components/global/NavigationDockContent/NavigationDockContent";
-import ContentTab from "../../components/react-components/global/ContentTab/ContentTab";
+import { useState } from 'react';
+import SideButtonMenu from '../../components/react-components/global/SideButtonMenu/SideButtonMenu';
+import ModelEditorCanvas from '../../components/react-components/model-editor/ModelEditorCanvas/ModelEditorCanvas';
+import IconButtonReact from '../../components/react-components/lit-wrappers/IconButtonReact';
+import PopUpBarReact from '../../components/react-components/lit-wrappers/PopUpBarReact';
+import NavigationDockContent from '../../components/react-components/global/NavigationDockContent/NavigationDockContent';
+import ContentTab from '../../components/react-components/global/ContentTab/ContentTab';
 
-import PlayIcon from "../../assets/icons/play_circle_filled-48px.svg";
-import FileIcon from "../../assets/icons/file_copy-48px.svg";
-import ModelIcon from "../../assets/icons/model-48px.svg";
-import ControlIcon from "../../assets/icons/control-enabled-48px.svg";
-import PhenotypeIcon from "../../assets/icons/phenotype-48px.svg";
-import DockIcon from "../../assets/icons/dock-arrow.svg";
-import ImportExportTabContent from "../../components/react-components/model-editor/ImportExportTabContent/ImportExportTabContent";
-import ModelEditorTabContent from "../../components/react-components/model-editor/ModelEditorTabContent/ModelEditorTabContent";
+import PlayIcon from '../../assets/icons/play_circle_filled-48px.svg';
+import FileIcon from '../../assets/icons/file_copy-48px.svg';
+import ModelIcon from '../../assets/icons/model-48px.svg';
+import ControlIcon from '../../assets/icons/control-enabled-48px.svg';
+import PhenotypeIcon from '../../assets/icons/phenotype-48px.svg';
+import DockIcon from '../../assets/icons/dock-arrow.svg';
+import ImportExportTabContent from '../../components/react-components/model-editor/ImportExportTabContent/ImportExportTabContent';
+import ModelEditorTabContent from '../../components/react-components/model-editor/ModelEditorTabContent/ModelEditorTabContent';
 
 type TabTypeME =
-  | "Start Computation"
-  | "Import/Export"
-  | "Model Editor"
-  | "Control-Enabled Editor"
-  | "Phenotype Editor"
+  | 'Start Computation'
+  | 'Import/Export'
+  | 'Model Editor'
+  | 'Control-Enabled Editor'
+  | 'Phenotype Editor'
   | null;
 
 const ModelEditor: React.FC = () => {
@@ -28,15 +28,15 @@ const ModelEditor: React.FC = () => {
 
   const renderTabContent = () => {
     switch (activeTab) {
-      case "Start Computation":
+      case 'Start Computation':
         return <div>Start Computation Content</div>;
-      case "Import/Export":
+      case 'Import/Export':
         return <ImportExportTabContent />;
-      case "Model Editor":
+      case 'Model Editor':
         return <ModelEditorTabContent />;
-      case "Control-Enabled Editor":
+      case 'Control-Enabled Editor':
         return <div>Control-Enabled Editor Content</div>;
-      case "Phenotype Editor":
+      case 'Phenotype Editor':
         return <div>Phenotype Editor Content</div>;
       default:
         return null;
@@ -56,40 +56,40 @@ const ModelEditor: React.FC = () => {
     <>
       <SideButtonMenu>
         <IconButtonReact
-          isActive={activeTab === "Start Computation"}
-          onClick={() => showHideTab("Start Computation")}
+          isActive={activeTab === 'Start Computation'}
+          onClick={() => showHideTab('Start Computation')}
           iconSrc={PlayIcon}
           iconAlt="Play"
           showTag={true}
           tagText="Start Computation"
         ></IconButtonReact>
         <IconButtonReact
-          isActive={activeTab === "Import/Export"}
-          onClick={() => showHideTab("Import/Export")}
+          isActive={activeTab === 'Import/Export'}
+          onClick={() => showHideTab('Import/Export')}
           iconSrc={FileIcon}
           iconAlt="File"
           showTag={true}
           tagText="Import/Export"
         ></IconButtonReact>
         <IconButtonReact
-          isActive={activeTab === "Model Editor"}
-          onClick={() => showHideTab("Model Editor")}
+          isActive={activeTab === 'Model Editor'}
+          onClick={() => showHideTab('Model Editor')}
           iconSrc={ModelIcon}
           iconAlt="Model"
           showTag={true}
           tagText="Model Editor"
         ></IconButtonReact>
         <IconButtonReact
-          isActive={activeTab === "Control-Enabled Editor"}
-          onClick={() => showHideTab("Control-Enabled Editor")}
+          isActive={activeTab === 'Control-Enabled Editor'}
+          onClick={() => showHideTab('Control-Enabled Editor')}
           iconSrc={ControlIcon}
           iconAlt="Control"
           showTag={true}
           tagText="Control-Enabled Editor"
         ></IconButtonReact>
         <IconButtonReact
-          isActive={activeTab === "Phenotype Editor"}
-          onClick={() => showHideTab("Phenotype Editor")}
+          isActive={activeTab === 'Phenotype Editor'}
+          onClick={() => showHideTab('Phenotype Editor')}
           iconSrc={PhenotypeIcon}
           iconAlt="Phenotype"
           showTag={true}
@@ -100,7 +100,7 @@ const ModelEditor: React.FC = () => {
       <ContentTab
         showTab={activeTab !== null}
         onClose={() => showHideTab(null)}
-        headerText={activeTab ?? ""}
+        headerText={activeTab ?? ''}
       >
         {renderTabContent()}
       </ContentTab>
