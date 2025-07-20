1eb587e410496fb8f6bcede72e8748b5e6fd72ab
MrMooshaber <vojtavesely1@seznam.cz>
2025-07-20 23:47:18 +0200

    feat: added arrows to the ComputationModeButton

--- src/components/react-components/model-editor/ComputationModeButton/ComputationModeButton.tsx
+++ src/components/react-components/model-editor/ComputationModeButton/ComputationModeButton.tsx
@@ -1,6 +1,9 @@
 import TextButtonReact from '../../lit-wrappers/TextButtonReact';
 import type { ComputationModeButtonProps } from './ComputationModeButtonProps';
 
+import RightArrow from '../../../../assets/icons/arrow-right.svg';
+import LeftArrow from '../../../../assets/icons/arrow-left.svg';
+
 const ComputationModeButton: React.FC<ComputationModeButtonProps> = ({
   active,
   onClick,
@@ -8,7 +11,7 @@ const ComputationModeButton: React.FC<ComputationModeButtonProps> = ({
 }) => {
   return (
     <div className="flex justify-center items-center h-[30px] w-full">
-      {active ? <img className='flex justify-center items-center h-[20px] w-[20px] bg-red-500' /> : null}
+      {active ? <img className='flex justify-center items-center h-[20px] w-[20px]' src={LeftArrow} alt="Left Arrow" /> : null}
       <TextButtonReact
         compHeight="100%"
         compWidth="70%"
@@ -16,7 +19,7 @@ const ComputationModeButton: React.FC<ComputationModeButtonProps> = ({
         handleClick={onClick}
         active={active}
       />
-      {active ? <img className='h-[20px] justify-center items-center w-[20px] bg-red-500' /> : null}
+      {active ? <img className='h-[20px] justify-center items-center w-[20px]' src={RightArrow} alt="Right Arrow" /> : null}
     </div>
   );
 };
