ef600cb6fc8782519018eb7360640c437404ca6c
MrMooshaber <vojtavesely1@seznam.cz>
2025-07-21 23:53:13 +0200

    feat: added config file

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+const config = {
+  version: '5.0.0 React',
+  computeEngine: {
+    /** This is default url for the compute engine. */
+    defaultURL: 'http://localhost:8000',
+  },
+};
+
+export default config;
