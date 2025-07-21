557356d12a48adac9852561174993d148aba2f37
MrMooshaber <vojtavesely1@seznam.cz>
2025-07-21 22:58:35 +0200

    feat: added props for the NavigationDockContent

--- src/components/react-components/global/NavigationDockContent/NavigationDockContent.tsx
+++ src/components/react-components/global/NavigationDockContent/NavigationDockContent.tsx
@@ -1,11 +1,13 @@
-import type { ReactNode } from "react";
 import IconButtonReact from "../../lit-wrappers/IconButtonReact";
 
 import ResultsIcon from "../../../../assets/icons/call_split-48px.svg";
 import EngineIcon from "../../../../assets/icons/engine-48px.svg";
+import type { NavigationDockContentProps } from "./NavigationDockContentProps";
 
-const NavigationDockContent: React.FC<{ children?: ReactNode }> = ({
+const NavigationDockContent: React.FC<NavigationDockContentProps> = ({
   children,
+  handleResultsClick,
+  handleComputeEngineClick,
 }) => {
   return (
     <div className="flex flex-row max-h-full h-full w-fit gap-3 p-1.5 overflow-y-hidden overflow-x-hidden justify-center items-center">
@@ -14,11 +16,13 @@ const NavigationDockContent: React.FC<{ children?: ReactNode }> = ({
           compHeight="80%"
           iconSrc={ResultsIcon}
           iconAlt="Results"
+          onClick={handleResultsClick}
         ></IconButtonReact>
 
         <IconButtonReact
           compHeight="80%"
           iconSrc={EngineIcon}
+          onClick={handleComputeEngineClick}
           iconAlt="Engine"
         ></IconButtonReact>
       </section>
