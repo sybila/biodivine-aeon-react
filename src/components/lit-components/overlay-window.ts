b5aea81529740cdb35a3f9d42bf44573bb20c978
MrMooshaber <vojtavesely1@seznam.cz>
2025-07-21 22:49:08 +0200

    chore: overlay-window component updated

--- src/components/lit-components/overlay-window.ts
+++ src/components/lit-components/overlay-window.ts
@@ -59,6 +59,9 @@ export class OverlayWindow extends LitElement {
   static styles = css`
     :host {
       display: inline-block;
+      position: fixed;
+      top: 0;
+      left: 0;
       height: var(--overlay-window-comp-height, 100vh);
       width: var(--overlay-window-comp-width, 100vw);
       max-height: var(--overlay-window-comp-max-height, 100vh);
