2dff492ed0e1bbbe5a6685289187ceaa683ed973
MrMooshaber <vojtavesely1@seznam.cz>
2025-07-29 17:40:48 +0200

    feat: added initial values for compute engine status

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,12 @@
+
+
 const config = {
   version: '5.0.0 React',
   computeEngine: {
+    /** Initial status of the compute engine. */
+    initialStatus: 'Disconnected',
+    /** Initial color of the compute engine status. */
+    initialStatusColor: 'red',
     /** This is default url for the compute engine. */
     defaultURL: 'http://localhost:8000',
   },
