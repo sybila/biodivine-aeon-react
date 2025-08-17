54a5551cc4731c90bef85e64948c30cee4e5354f
MrMooshaber <vojtavesely1@seznam.cz>
2025-08-17 20:48:47 +0200

    feat:No computed data moved under Stability Analysis Results header

--- src/components/react-components/attractor-bifurcation-explorer/StabilityAnalysisTabContent/StabilityAnalysisTable/StabilityAnalysisTable.tsx
+++ src/components/react-components/attractor-bifurcation-explorer/StabilityAnalysisTabContent/StabilityAnalysisTable/StabilityAnalysisTable.tsx
@@ -8,20 +8,21 @@ const StabilityAnalysisTable = () => {
     (state) => state.stabilityData
   );
 
-  if (!stabilityResults) {
+  const renderNoResults = () => {
     return (
-      <div className="h-[200px] w-full flex justify-center items-center">
+      <div className="max-h-[100px] h-[100px] md:max-h-[150px] md:h-[150px] w-full flex justify-center items-center">
         <SimpleHeaderReact
           headerText="No Computed Data"
           textFontWeight="normal"
         />
       </div>
     );
-  }
+  };
+
   const renderTable = () => {
     return (
       <section className="flex flex-col w-full max-h-[100px] md:max-h-[200px] xl:max-h-[300px] 2xl:max-h-[400px] gap-2 overflow-y-auto">
-        {stabilityResults.map((variableStabilityData, index) => (
+        {stabilityResults?.map((variableStabilityData, index) => (
           <StabilityAnalysisTableRow key={index} {...variableStabilityData} />
         ))}
       </section>
@@ -30,13 +31,13 @@ const StabilityAnalysisTable = () => {
 
   return (
     <div className="h-fit w-full flex flex-col justify-center items-start gap-1">
-        <DotHeaderReact
-          headerText="Stability Analysis Results"
-          compHeight="30px"
-          compWidth="100%"
-          justifyHeader="start"
-        />
-      {renderTable()}
+      <DotHeaderReact
+        headerText="Stability Analysis Results"
+        compHeight="30px"
+        compWidth="100%"
+        justifyHeader="start"
+      />
+      {!stabilityResults ? renderNoResults() : renderTable()}
     </div>
   );
 };
