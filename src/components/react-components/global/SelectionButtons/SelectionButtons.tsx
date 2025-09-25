3f78b12eab03c914f9201550156997014c6a9238
MrMooshaber <vojtavesely1@seznam.cz>
2025-09-25 22:10:34 +0200

    feat: created SelectionButtons component

---
+++ src/components/react-components/global/SelectionButtons/SelectionButtons.tsx
@@ -0,0 +1,68 @@
+import DeselectAllIcon from '../../../../assets/icons/deselect_all.svg';
+import ToggleSelectionIcon from '../../../../assets/icons/toggle_selection.svg';
+import SelectAllIcon from '../../../../assets/icons/select_all.svg';
+
+import type { SelectionButtonsProps } from './SelectionButtonsProps';
+import IconButtonReact from '../../lit-wrappers/IconButtonReact';
+
+const SelectionButtons: React.FC<SelectionButtonsProps> = ({
+  variableNames,
+  selectedVariables,
+  setSelectedVariables,
+  buttonBorderRadius = '10px',
+  buttonSize = '29px',
+}) => {
+  const selectAll = () => {
+    setSelectedVariables(
+      variableNames.reduce((acc, name) => {
+        acc[name] = true;
+        return acc;
+      }, {} as Record<string, boolean>)
+    );
+  };
+
+  const toggleSelected = () => {
+    setSelectedVariables(
+      variableNames.reduce((acc, name) => {
+        if (!selectedVariables[name]) {
+          acc[name] = !selectedVariables[name];
+        }
+        return acc;
+      }, {} as Record<string, boolean>)
+    );
+  };
+
+  const deselectAll = () => {
+    setSelectedVariables({});
+  };
+
+  /** Array of buttons for changing the selection status of variables.
+   * Each button is represented as a tuple containing:
+   * - the button icon (string)
+   * - The icon alt (string)
+   * - The onClick handler function (() => void)
+   */
+  const statusButtons: Array<[string, string, () => void]> = [
+    [DeselectAllIcon, 'D', () => deselectAll()],
+    [ToggleSelectionIcon, 'T', () => toggleSelected()],
+    [SelectAllIcon, 'S', () => selectAll()],
+  ];
+
+  return (
+    <div className="flex flex-row gap-2 h-full max-w-[50%] items-center justify-start">
+      {statusButtons.map(([icon, alt, onClick], index) => (
+        <IconButtonReact
+          key={index}
+          compHeight={buttonSize}
+          compWidth={buttonSize}
+          buttonBorderRadius={buttonBorderRadius}
+          iconSrc={icon}
+          iconAlt={alt}
+          handleClick={onClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SelectionButtons;
---
+++ src/components/react-components/global/SelectionButtons/SelectionButtonsProps.ts
@@ -0,0 +1,9 @@
+export type SelectionButtonsProps = {
+  variableNames: string[];
+  selectedVariables: Record<string, boolean>;
+  setSelectedVariables: (selectedVariables: Record<string, boolean>) => void;
+  /** Size of the buttons border radius in the form of Css length (e.g. '8px', '0.5rem') */
+  buttonBorderRadius?: string;
+  /** Size of the buttons in the form of Css length (e.g. '29px', '1.5rem') */
+  buttonSize?: string;
+};
