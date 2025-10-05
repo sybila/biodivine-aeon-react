85a7884719716f211e33f04fa55b8a773ee56040
Samuel Pastva <sam.pastva@gmail.com>
2025-10-05 21:27:32 +0200

    feat: Allow vite and router to be configured with basepath, plus switch to hash-based history.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,16 @@
-import { createRouter } from '@tanstack/react-router';
+import { createHashHistory, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routes';
 
-const router = createRouter({ routeTree });
+
+// Allows navigation without changing the URL.
+// For final deployment, we can use normal URL navigation,
+// but this makes it easier to deploy as a static site.
+const hashHistory = createHashHistory()
+const router = createRouter({ 
+  routeTree,
+  basepath: import.meta.env.BASE_URL,
+  history: hashHistory
+});
 
 declare module '@tanstack/react-router' {
   interface Register {
