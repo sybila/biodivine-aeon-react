210ea61eb624d1295be2b486c7c269b42cd3525b
MrMooshaber <vojtavesely1@seznam.cz>
2026-02-23 17:00:54 +0100

    feat: updated documentation

--- src/services/global/LiveModel/LiveModelInt.ts
+++ src/services/global/LiveModel/LiveModelInt.ts
@@ -11,7 +11,7 @@ import type { VariablesLMInt } from './VariablesLM/VariablesLMInt';
 	Stores the PBN currently loaded into the editor. This is what you should interact with when
 	you want to modify the model, not the editor or graph directly.
 
-	It is the responsibility of the `LiveModel` to always update `ModelEditor` and `CytoscapeME`
+	It is the responsibility of the `LiveModel` to always update `ModelEditor` and `ModelVisualization`
 	to reflect the current state of the model.
 */
 export interface LiveModelInt {
