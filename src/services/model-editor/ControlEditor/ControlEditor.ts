cec2f79bea3c12d6d45913aaef3e11446afb6ec4
MrMooshaber <vojtavesely1@seznam.cz>
2026-03-01 20:08:45 +0100

    feat: stores and services isolated from ControlEditor service

--- src/services/model-editor/ControlEditor/ControlEditor.ts
+++ src/services/model-editor/ControlEditor/ControlEditor.ts
@@ -1,13 +1,19 @@
+import type { ControlStatus } from '../../../stores/LiveModel/ControlStore/ControlStatus';
 import useControlStore from '../../../stores/LiveModel/ControlStore/useControlStore';
 import useVariablesStore from '../../../stores/LiveModel/VariablesStore/useVariablesStore';
+import type { VariablesStatus } from '../../../stores/LiveModel/VariablesStore/VariablesStatus';
+import type { ModelEditorStatus } from '../../../stores/ModelEditor/ModelEditorStatus';
 import useModelEditorStatus from '../../../stores/ModelEditor/useModelEditorStatus';
+import type { ZustandStore } from '../../../stores/ZustandStoreType';
 import type { ControlInfo, Oscillation, Phenotype } from '../../../types';
 import { LiveModel } from '../../global/LiveModel/LiveModel';
+import type { LiveModelInt } from '../../global/LiveModel/LiveModelInt';
 import CytoscapeME from '../ModelVisualization/CytoscapeME';
+import type { ModelVisualizationInt } from '../ModelVisualization/ModelVisualizationInt';
 import type { ControlEditorInt } from './ControlEditorInt';
 
 class ControlEditorClass implements ControlEditorInt {
-  // #region --- Properties ---
+  // #region --- Properties + Constructor ---
 
   /** Record containing all selected variables in the ControlEditorTabContent.tsx component.
    *  Key: variable name
@@ -18,6 +24,27 @@ class ControlEditorClass implements ControlEditorInt {
 
   /** Currently searched variable name in the ControlEditorTabContent.tsx component */
   private variableSearch: string = '';
+
+  private modelVisualizationServ: ModelVisualizationInt;
+  private liveModelServ: LiveModelInt;
+
+  private controlStore: ZustandStore<ControlStatus>;
+  private variablesStore: ZustandStore<VariablesStatus>;
+  private modelEditorStatusStore: ZustandStore<ModelEditorStatus>;
+
+  constructor(
+    modelVisualizationServ: ModelVisualizationInt,
+    liveModelServ: LiveModelInt,
+    controlStore: ZustandStore<ControlStatus>,
+    variablesStore: ZustandStore<VariablesStatus>,
+    modelEditorStatusStore: ZustandStore<ModelEditorStatus>
+  ) {
+    this.modelVisualizationServ = modelVisualizationServ;
+    this.liveModelServ = liveModelServ;
+    this.controlStore = controlStore;
+    this.variablesStore = variablesStore;
+    this.modelEditorStatusStore = modelEditorStatusStore;
+  }
 
   // #endregion
 
@@ -46,7 +73,7 @@ class ControlEditorClass implements ControlEditorInt {
    * (you must first set hoverVariableInfo with setHoverVariableFunction before running this function)
    */
   public hoverVariable(id: number, turnOnHover: boolean) {
-    useModelEditorStatus
+    this.modelEditorStatusStore
       .getState()
       .setHoverItemInfo(turnOnHover ? { type: 'variable', id } : null);
   }
@@ -71,17 +98,17 @@ class ControlEditorClass implements ControlEditorInt {
 
   /** Changes the control enabled state of a variable by its ID */
   public changeControlEnabled(id: number, enabled: boolean) {
-    LiveModel.Control.changeControlEnabledById(id, enabled);
+    this.liveModelServ.Control.changeControlEnabledById(id, enabled);
   }
 
   /** Toggles the control enabled state of a variable by its ID */
   public toggleControlEnabled(id: number) {
-    const controlInfo: ControlInfo | undefined = useControlStore
+    const controlInfo: ControlInfo | undefined = this.controlStore
       .getState()
       .getVariableControlInfo(id);
 
     if (controlInfo) {
-      LiveModel.Control.changeControlEnabledById(
+      this.liveModelServ.Control.changeControlEnabledById(
         id,
         !controlInfo.controlEnabled
       );
@@ -104,7 +131,7 @@ class ControlEditorClass implements ControlEditorInt {
     selectedVariables.forEach(([varName, isSelected]) => {
       if (!isSelected) return;
 
-      const variableId = useVariablesStore
+      const variableId = this.variablesStore
         .getState()
         .variableFromName(varName)?.id;
 
@@ -120,12 +147,12 @@ class ControlEditorClass implements ControlEditorInt {
 
   /** Changes the phenotype state of a variable by its ID */
   public changePhenotype(id: number, phenotype: Phenotype) {
-    LiveModel.Control.changePhenotypeById(id, phenotype);
+    this.liveModelServ.Control.changePhenotypeById(id, phenotype);
   }
 
   /** Toggles the phenotype state of a variable by its ID */
   public togglePhenotype(id: number) {
-    const controlInfo: ControlInfo | undefined = useControlStore
+    const controlInfo: ControlInfo | undefined = this.controlStore
       .getState()
       .getVariableControlInfo(id);
 
@@ -133,13 +160,13 @@ class ControlEditorClass implements ControlEditorInt {
 
     switch (controlInfo.phenotype) {
       case true:
-        LiveModel.Control.changePhenotypeById(id, false);
+        this.liveModelServ.Control.changePhenotypeById(id, false);
         break;
       case false:
-        LiveModel.Control.changePhenotypeById(id, null);
+        this.liveModelServ.Control.changePhenotypeById(id, null);
         break;
       default:
-        LiveModel.Control.changePhenotypeById(id, true);
+        this.liveModelServ.Control.changePhenotypeById(id, true);
     }
   }
 
@@ -159,7 +186,7 @@ class ControlEditorClass implements ControlEditorInt {
     selectedVariables.forEach(([varName, isSelected]) => {
       if (!isSelected) return;
 
-      const variableId = useVariablesStore
+      const variableId = this.variablesStore
         .getState()
         .variableFromName(varName)?.id;
 
@@ -175,12 +202,12 @@ class ControlEditorClass implements ControlEditorInt {
 
   /** Returns the currently set phenotype oscillation state in the ControlEditorTabContent.tsx component */
   public getPhenotypeOscillation(): Oscillation {
-    return LiveModel.Control.getOscillation();
+    return this.liveModelServ.Control.getOscillation();
   }
 
   /** Sets the currently set phenotype oscillation state in the ControlEditorTabContent.tsx component */
   public setPhenotypeOscillation(newOscillation: Oscillation) {
-    LiveModel.Control.setOscillation(newOscillation);
+    this.liveModelServ.Control.setOscillation(newOscillation);
   }
 
   // #endregion
@@ -191,13 +218,19 @@ class ControlEditorClass implements ControlEditorInt {
    * If `turnOnHover` is true, it starts the hover effect; if false, it ends it.
    */
   public hoverVariableCytoscape(id: number, turnOnHover: boolean) {
-    CytoscapeME.hoverNode(id, turnOnHover);
+    this.modelVisualizationServ.hoverNode(id, turnOnHover);
   }
 
   // #endregion
 }
 
 /** Singleton for controlling the behavior of the Control Editor */
-const ControlEditor: ControlEditorClass = new ControlEditorClass();
+const ControlEditor: ControlEditorClass = new ControlEditorClass(
+  CytoscapeME,
+  LiveModel,
+  useControlStore,
+  useVariablesStore,
+  useModelEditorStatus
+);
 
 export default ControlEditor;
