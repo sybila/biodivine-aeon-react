e7576585a8ff36f157b8f895606fa2748441a6b9
MrMooshaber <vojtavesely1@seznam.cz>
2026-03-01 21:49:58 +0100

    feat: TabOperations converted into singleton + implements TabOperationsInt interface

--- src/services/global/Navigation/TabOperations.ts
+++ src/services/global/Navigation/TabOperations.ts
@@ -1,22 +1,23 @@
 import type { TabType } from '../../../types';
 
-import ModelEditorIcon from '../../../assets/icons/model-editor.svg';
 import AttractorBifurcationExplorerIcon from '../../../assets/icons/attractor-bifurcation-explorer.svg';
 import AttractorVisualizerIcon from '../../../assets/icons/attractor-visualizer.svg';
-import WitnessIcon from '../../../assets/icons/witness.svg';
 import ControlPerturbationsTableIcon from '../../../assets/icons/control-perturbations-table.svg';
+import ModelEditorIcon from '../../../assets/icons/model-editor.svg';
+import WitnessIcon from '../../../assets/icons/witness.svg';
+import type { TabOperationsInt } from './TabOperationsInt';
 
-/** Class containing static methods over navigation tabs */
-class TabOperations {
+/** Class containing methods for operations on navigation tabs */
+class TabOperationsClass implements TabOperationsInt {
   // #region --- Properties ---
 
   /** A set of tab types that can only be opened as a single instance. */
-  private static singleTabTypes: Set<TabType> = new Set<TabType>([
+  private singleTabTypes: Set<TabType> = new Set<TabType>([
     'Attractor Bifurcation Explorer',
     'Control Perturbations Table',
   ]);
 
-  private static tabTypeToIcon: Record<TabType, string> = {
+  private tabTypeToIcon: Record<TabType, string> = {
     'Model Editor': ModelEditorIcon,
     'Attractor Bifurcation Explorer': AttractorBifurcationExplorerIcon,
     'Attractor Visualizer': AttractorVisualizerIcon,
@@ -27,14 +28,16 @@ class TabOperations {
   // #endregion
 
   /** Determines if more than one instance of a tab can be opened. */
-  public static canOpenMoreThanOne(tabType: TabType): boolean {
+  public canOpenMoreThanOne(tabType: TabType): boolean {
     return !this.singleTabTypes.has(tabType);
   }
 
   /** Returns the icon for a given tab type */
-  public static getTabTypeIcon(tabType: TabType): string {
+  public getTabTypeIcon(tabType: TabType): string {
     return this.tabTypeToIcon[tabType] ?? '';
   }
 }
+
+const TabOperations = new TabOperationsClass();
 
 export default TabOperations;
