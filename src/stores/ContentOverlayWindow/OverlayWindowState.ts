a34a7b55741e13003540ba8ac9c2fa0469650e3a
MrMooshaber <vojtavesely1@seznam.cz>
2026-03-01 18:33:34 +0100

    feat: useOVerlayWindowStore separated into two files

---
+++ src/stores/ContentOverlayWindow/OverlayWindowState.ts
@@ -0,0 +1,14 @@
+export type OverlayWindowState = {
+  /** The current content to be displayed in the overlay window.
+   * If null, the overlay window is hidden.
+   */
+  currentContent: { header: string; content: React.ReactNode } | null;
+  /** Sets the current content to be displayed in the overlay window.
+   * If null, the overlay window is hidden.
+   * @param content - The content to be displayed in the overlay window, or null to hide it.
+   */
+  setCurrentContent: (
+    content: { header: string; content: React.ReactNode } | null
+  ) => void;
+  clear: () => void;
+};
--- src/stores/ContentOverlayWindow/useOverlayWindowStore.tsx
+++ src/stores/ContentOverlayWindow/useOverlayWindowStore.tsx
@@ -1,19 +1,5 @@
 import { create } from 'zustand';
-
-type OverlayWindowState = {
-  /** The current content to be displayed in the overlay window.
-   * If null, the overlay window is hidden.
-   */
-  currentContent: { header: string; content: React.ReactNode } | null;
-  /** Sets the current content to be displayed in the overlay window.
-   * If null, the overlay window is hidden.
-   * @param content - The content to be displayed in the overlay window, or null to hide it.
-   */
-  setCurrentContent: (
-    content: { header: string; content: React.ReactNode } | null
-  ) => void;
-  clear: () => void;
-};
+import type { OverlayWindowState } from './OverlayWindowState';
 
 /** Zustand store for managing the content of global content overlay window in the model editor. */
 const useOverlayWindowStore = create<OverlayWindowState>()((set) => ({
