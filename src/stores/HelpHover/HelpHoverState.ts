2c670bd40ad90157c9afe5c701bbab7e74563928
MrMooshaber <vojtavesely1@seznam.cz>
2026-03-06 23:01:47 +0100

    feat: useHelpHoverStore separated into two parts

---
+++ src/stores/HelpHover/HelpHoverState.ts
@@ -0,0 +1,23 @@
+export type HelpHoverState = {
+  /**
+   * Coordinates for the help hover, or null if the hover is hidden.
+   * x is the horizontal offset in pixels from the viewport left.
+   * y is the vertical offset in pixels from the viewport top.
+   */
+  position: { x: number; y: number } | null;
+  /** The current content to be displayed in the help hover.
+   * If null, the help hover is hidden.
+   */
+  helpText: string | null;
+  /** Set the help hover state.
+   *  @param event (MouseEvent) - event to get the position from
+   *  @param helpText (string) - text to be displayed in the help hover
+   */
+  setHelpHover(
+    event: MouseEvent,
+    helpText: string,
+    adjustTop?: number,
+    adjustLeft?: number
+  ): void;
+  clear: () => void;
+};
--- src/stores/HelpHover/useHelpHoverStore.ts
+++ src/stores/HelpHover/useHelpHoverStore.ts
@@ -1,28 +1,5 @@
 import { create } from 'zustand';
-
-type HelpHoverState = {
-  /**
-   * Coordinates for the help hover, or null if the hover is hidden.
-   * x is the horizontal offset in pixels from the viewport left.
-   * y is the vertical offset in pixels from the viewport top.
-   */
-  position: { x: number; y: number } | null;
-  /** The current content to be displayed in the help hover.
-   * If null, the help hover is hidden.
-   */
-  helpText: string | null;
-  /** Set the help hover state.
-   *  @param event (MouseEvent) - event to get the position from
-   *  @param helpText (string) - text to be displayed in the help hover
-   */
-  setHelpHover(
-    event: MouseEvent,
-    helpText: string,
-    adjustTop?: number,
-    adjustLeft?: number
-  ): void;
-  clear: () => void;
-};
+import type { HelpHoverState } from './HelpHoverState';
 
 /** Zustand store for managing the help hover in the application. */
 const useHelpHoverStore = create<HelpHoverState>()((set) => ({
