0573996b0f11428fda1aac1062acbe9683e8bb0b
MrMooshaber <vojtavesely1@seznam.cz>
2026-03-06 23:03:59 +0100

    feat: StoresProviderInt interface defined

---
+++ src/wiring/StoresProvider/StoresProviderInt.ts
@@ -0,0 +1,56 @@
+import type { BifurcationExplorerStatusState } from '../../stores/AttractorBifurcationExplorer/BifurcationExplorerStatusState';
+import type { AttractorVisualizerStatusState } from '../../stores/AttractorVisualizer/AttractorVisualizerStatusState';
+import type { ComputeEngineStatusState } from '../../stores/ComputationManager/ComputeEngineStatusStore/ComputeEngineStatusState';
+import type { ResultsStatus } from '../../stores/ComputationManager/ResultStatus/ResultStatus';
+import type { OverlayWindowState } from '../../stores/ContentOverlayWindow/OverlayWindowState';
+import type { PerturbationFiltersSortState } from '../../stores/ControlPerturbationsTable/PerturbationsFilterSortStore/PerturbationsFilterSortState';
+import type { HelpHoverState } from '../../stores/HelpHover/HelpHoverState';
+import type { ControlStatus } from '../../stores/LiveModel/ControlStore/ControlStatus';
+import type { ModelState } from '../../stores/LiveModel/LoadedModelStore/ModelState';
+import type { ModelInfoState } from '../../stores/LiveModel/ModelInfoStore/ModelInfoState';
+import type { RegulationsStatus } from '../../stores/LiveModel/RegulationsStore/RegulationsStatus';
+import type { UpdateFunctionsState } from '../../stores/LiveModel/UpdateFunctionsStore/UpdateFunctionsState';
+import type { VariablesStatus } from '../../stores/LiveModel/VariablesStore/VariablesStatus';
+import type { ModelEditorStatus } from '../../stores/ModelEditor/ModelEditorStatus';
+import type { TabsState } from '../../stores/Navigation/TabState';
+import type { WarningState } from '../../stores/Warning/WarningState';
+import type { ZustandStore } from '../../stores/ZustandStoreType';
+
+/** Interface which defines provider for all stores used inside the app. */
+export interface StoresProviderInt {
+  // #region --- Page Status Stores ---
+
+  bifurcationExplorerStatusStore: ZustandStore<BifurcationExplorerStatusState>;
+  attractorVisualizerStatusStore: ZustandStore<AttractorVisualizerStatusState>;
+  modelEditorStatusStore: ZustandStore<ModelEditorStatus>;
+  perturbationFiltersSortStore: ZustandStore<PerturbationFiltersSortState>;
+
+  // #endregion
+
+  // #region --- Computation Manager ---
+
+  computeEngineStatusStore: ZustandStore<ComputeEngineStatusState>;
+  resultsStatusStore: ZustandStore<ResultsStatus>;
+
+  // #endregion
+
+  // #region --- Global Stores ---
+
+  tabsStore: ZustandStore<TabsState>;
+  warningStore: ZustandStore<WarningState>;
+  overlayWindowStore: ZustandStore<OverlayWindowState>;
+  helpHoverStore: ZustandStore<HelpHoverState>;
+
+  // #endregion
+
+  // #region --- Live Model ---
+
+  loadedModelStore: ZustandStore<ModelState>;
+  modelInfoStore: ZustandStore<ModelInfoState>;
+  variablesStore: ZustandStore<VariablesStatus>;
+  regulationsStore: ZustandStore<RegulationsStatus>;
+  updateFunctionsStore: ZustandStore<UpdateFunctionsState>;
+  controlStore: ZustandStore<ControlStatus>;
+
+  // #endregion
+}
