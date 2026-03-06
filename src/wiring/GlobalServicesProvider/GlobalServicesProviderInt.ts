43532ffc8170c526da75f2c007555510b477b1dd
MrMooshaber <vojtavesely1@seznam.cz>
2026-03-06 23:04:21 +0100

    feat: GlobalServicesProviderInt interface defined

---
+++ src/wiring/GlobalServicesProvider/GlobalServicesProviderInt.ts
@@ -0,0 +1,14 @@
+import type { ComputationManagerInt } from '../../services/global/ComputationManager/ComputationManagerInt';
+import type { LiveModelInt } from '../../services/global/LiveModel/LiveModelInt';
+import type { TabOperationsInt } from '../../services/global/Navigation/TabOperationsInt';
+import type { ResultsOperationsInt } from '../../services/global/ResultsOperations/ResultsOperationsInt';
+import type { WarningInt } from '../../services/global/Warning/WarningInt';
+
+/** Interfaces which defines the provider of global services. */
+export interface GlobalServicesProviderInt {
+  computationManagerServ: ComputationManagerInt;
+  liveModelServ: LiveModelInt;
+  tabOperationsServ: TabOperationsInt;
+  resultsOperationsServ: ResultsOperationsInt;
+  warningServ: WarningInt;
+}
