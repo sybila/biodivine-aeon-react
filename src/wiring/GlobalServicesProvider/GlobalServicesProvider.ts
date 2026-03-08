16d05ec25b491e81dac1f63630f8d1c29de2ec12
MrMooshaber <vojtavesely1@seznam.cz>
2026-03-08 17:20:51 +0100

    feat: ComputationManager converted from singleton into normal class

--- src/wiring/GlobalServicesProvider/GlobalServicesProvider.ts
+++ src/wiring/GlobalServicesProvider/GlobalServicesProvider.ts
@@ -1,9 +1,22 @@
+import ComputationManager from '../../services/global/ComputationManager/ComputationManager';
+import type { ComputationManagerInt } from '../../services/global/ComputationManager/ComputationManagerInt';
+import type { StoresProviderInt } from '../StoresProvider/StoresProviderInt';
+
 class GlobalServicesProvider {
-  //   public computationManagerServ: ComputationManagerInt;
+  public computationManagerServ: ComputationManagerInt;
   //   public liveModelServ: LiveModelInt;
   //   public tabOperationsServ: TabOperationsInt;
   //   public resultsOperationsServ: ResultsOperationsInt;
   //   public warningServ: WarningInt;
 
-  constructor() {}
+  constructor(storesProvider: StoresProviderInt) {
+    this.computationManagerServ = new ComputationManager(
+      storesProvider.bifurcationExplorerStatusStore,
+      storesProvider.resultsStatusStore,
+      storesProvider.computeEngineStatusStore,
+      storesProvider.updateFunctionsStore,
+      storesProvider.variablesStore,
+      storesProvider.tabsStore
+    );
+  }
 }
