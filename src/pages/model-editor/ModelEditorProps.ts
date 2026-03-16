cc2fe6f64651f29f7a852478f78c176009379ec9
MrMooshaber <vojtavesely1@seznam.cz>
2026-03-16 18:04:21 +0100

    feat: messageServ added into ModelEditorProps

--- src/pages/model-editor/ModelEditorProps.ts
+++ src/pages/model-editor/ModelEditorProps.ts
@@ -1,5 +1,6 @@
 import type { ComputationManagerInt } from '../../services/global/ComputationManager/ComputationManagerInt';
 import type { LiveModelInt } from '../../services/global/LiveModel/LiveModelInt';
+import type { MessageInt } from '../../services/global/Message/MessageInt';
 import type { WarningInt } from '../../services/global/Warning/WarningInt';
 import type { ControlEditorInt } from '../../services/model-editor/ControlEditor/ControlEditorInt';
 import type { ModelEditorInt } from '../../services/model-editor/ModelEditor/ModelEditorInt';
@@ -28,6 +29,7 @@ export type ModelEditorProps = {
   searchAndFilterHelpersServ: SearchAndFilterHelpersInt;
   fileConvertorsServ: FileConvertorsInt;
   warningServ: WarningInt;
+  messageServ: MessageInt;
 
   // # endregion
 
