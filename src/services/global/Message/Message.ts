e81417a8d942efb67e2e20bed64956995a38c2e6
MrMooshaber <vojtavesely1@seznam.cz>
2026-03-16 14:42:48 +0100

    feat: Message service implemented

---
+++ src/services/global/Message/Message.ts
@@ -0,0 +1,32 @@
+import type { MessageInt } from './MessageInt';
+
+/** Class which implements global message management. */
+class Message implements MessageInt {
+  private successFunction: (message: string, duration?: number) => void;
+  private infoFunction: (message: string, duration?: number) => void;
+  private errorFunction: (message: string, duration?: number) => void;
+
+  constructor(
+    successFunction: (message: string, duration?: number) => void,
+    infoFunction: (message: string, duration?: number) => void,
+    errorFunction: (message: string, duration?: number) => void
+  ) {
+    this.successFunction = successFunction;
+    this.infoFunction = infoFunction;
+    this.errorFunction = errorFunction;
+  }
+
+  showSuccess(message: string, duration: number = 3000): void {
+    this.successFunction(message, duration);
+  }
+
+  showInfo(message: string, duration: number = 3000): void {
+    this.infoFunction(message, duration);
+  }
+
+  showError(message: string, duration: number = 3000): void {
+    this.errorFunction(message, duration);
+  }
+}
+
+export default Message;
