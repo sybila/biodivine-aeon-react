3626979784923892a10c353b241ef584e6f6e636
MrMooshaber <vojtavesely1@seznam.cz>
2026-03-24 15:35:32 +0100

    feat: added documentation for the setPositionOfAllVariables function

--- src/stores/LiveModel/VariablePositions/VariablePostionsState.ts
+++ src/stores/LiveModel/VariablePositions/VariablePostionsState.ts
@@ -8,7 +8,7 @@ export type VariablePositionsState = {
   setVariablePosition: (variableId: number, position: Position) => void;
   /** Remove the position of a variable node */
   removeVariablePosition: (variableId: number) => void;
-  /**  */
+  /** Sets position of all variables at once. */
   setPositionOfAllVariables: (positions: Record<number, Position>) => void;
   /** Function to clear all variable positions */
   clear: () => void;
