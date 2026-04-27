8849c4a9ad86628a369b5363c5a90bd57214bdf1
MrMooshaber <vojtavesely1@seznam.cz>
2026-04-27 20:26:56 +0200

    fix: overflow fixes

--- src/components/react-components/global/HelpTabContent/HelpTabContent.tsx
+++ src/components/react-components/global/HelpTabContent/HelpTabContent.tsx
@@ -3,7 +3,7 @@ import type { HelpTabContentProps } from './HelpTabContentProps';
 
 const HelpTabContent: React.FC<HelpTabContentProps> = ({ text }) => {
   return (
-    <div className="flex flex-col items-center w-full h-fit gap-3">
+    <div className="flex flex-col items-center w-full max-h-[64vh] overflow-auto gap-3">
       <MarkdownText height="fit-content" width="100%" text={text} />
     </div>
   );
