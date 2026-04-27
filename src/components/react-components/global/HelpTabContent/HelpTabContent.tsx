ba3c891dc47f12e9540427016b4b3a4c429c2967
MrMooshaber <vojtavesely1@seznam.cz>
2026-04-27 17:55:39 +0200

    feat: added react markdown

--- src/components/react-components/global/HelpTabContent/HelpTabContent.tsx
+++ src/components/react-components/global/HelpTabContent/HelpTabContent.tsx
@@ -1,9 +1,12 @@
+import ReactMarkdown from 'react-markdown';
 import type { HelpTabContentProps } from './HelpTabContentProps';
 
 const HelpTabContent: React.FC<HelpTabContentProps> = ({ text }) => {
   return (
     <div className="flex flex-col items-center w-full h-fit gap-3">
-      <span className="text-[20px] text-center">{text}</span>
+      <div className="text-[20px] text-center">
+        <ReactMarkdown>{text}</ReactMarkdown>
+      </div>
     </div>
   );
 };
