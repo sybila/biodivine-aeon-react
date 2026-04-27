b80e9ea7dc80e5c6197b83e90260d8ff31bdbb0b
MrMooshaber <vojtavesely1@seznam.cz>
2026-04-27 19:05:41 +0200

    feat: added documentation to HelpTextInt interface

--- src/services/global/StringProvider/HelpTexts/HelpTextsInt.ts
+++ src/services/global/StringProvider/HelpTexts/HelpTextsInt.ts
@@ -1,3 +1,4 @@
+/** Class providing help texts for various UI components. These help texts should be written in markdown format. */
 export interface HelpTextsInt {
   modelEditor(): string;
   attractorVisualizer(): string;
