22f383883ec002a29ddaade7b1fd3165a7cdbc61
MrMooshaber <vojtavesely1@seznam.cz>
2026-06-07 22:28:47 +0200

    feat: add OtherStrings class into StringProvider

---
+++ src/services/global/StringProvider/OtherStrings/OtherStringsInt.ts
@@ -0,0 +1,7 @@
+import type { ModelEditorOtherStringsInt } from './ModelEditorOtherStrings/ModelEditorOtherStringsInt';
+
+/** Class which provides uncategorized strings (not tooltips, help texts....) for different parts of the app.  */
+export interface OtherStringsInt {
+  /** Placeholder for model editor page. */
+  ModelEditorOtherStrings: ModelEditorOtherStringsInt;
+}
--- src/services/global/StringProvider/StringProvider.ts
+++ src/services/global/StringProvider/StringProvider.ts
@@ -1,5 +1,7 @@
 import HelpTexts from './HelpTexts/HelpTexts';
 import type { HelpTextsInt } from './HelpTexts/HelpTextsInt';
+import OtherStrings from './OtherStrings/OtherStrings';
+import type { OtherStringsInt } from './OtherStrings/OtherStringsInt';
 import type { StringProviderInt } from './StringProviderInt';
 import ToolTips from './ToolTips/ToolTips';
 import type { ToolTipsInt } from './ToolTips/ToolTipsInt';
@@ -7,10 +9,12 @@ import type { ToolTipsInt } from './ToolTips/ToolTipsInt';
 class StringProvider implements StringProviderInt {
   public ToolTips: ToolTipsInt;
   public HelpTexts: HelpTextsInt;
+  public OtherStrings: OtherStringsInt;
 
   constructor() {
     this.ToolTips = new ToolTips();
     this.HelpTexts = new HelpTexts();
+    this.OtherStrings = new OtherStrings();
   }
 }
 
