81db62b147ad7db2519e492de655fc8ec31148da
MrMooshaber <vojtavesely1@seznam.cz>
2026-09-01 22:23:37 +0200

    feat: implemented AeonFormat utility class

---
+++ src/services/utilities/AeonFormat/AeonFormatInt.ts
@@ -0,0 +1,38 @@
+export interface AeonFormatInt {
+  // #region --- Model Description  ---
+  getRegexModelDescription(): RegExp;
+  // #endregion
+
+  // #region --- Model Name  ---
+  getRegexModelName(): RegExp;
+  // #endregion
+
+  // #region --- Position  ---
+  getRegexPosition(): RegExp;
+  // #endregion
+
+    // #region --- Regulation  ---
+  getRegexRegulation(): RegExp;
+  // #endregion
+
+  // #region --- Update Function  ---
+  getRegexUpdateFunction(): RegExp;
+  // #endregion
+
+  // #region --- Control  ---
+  getRegexControl(): RegExp;
+  // #endregion
+
+  // #region --- Phenotype  ---
+  getRegexPhenotypePrefix(): RegExp;
+  getRegexPhenotypeVariable(): RegExp;
+  // #endregion
+
+  // #region --- Results  ---
+  getRegexResults(): RegExp;
+  // #endregion
+
+  // #region --- Comment ---
+  getRegexComment(): RegExp;
+  // #endregion
+}
