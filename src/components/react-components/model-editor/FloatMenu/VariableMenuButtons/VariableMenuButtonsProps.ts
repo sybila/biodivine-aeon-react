3f6af0fe2f7cbc7824a6033bf087b651275cb268
MrMooshaber <vojtavesely1@seznam.cz>
2026-09-17 16:54:45 +0200

    fix: missing import in VariableMenuButtonsProps

--- src/components/react-components/model-editor/FloatMenu/VariableMenuButtons/VariableMenuButtonsProps.ts
+++ src/components/react-components/model-editor/FloatMenu/VariableMenuButtons/VariableMenuButtonsProps.ts
@@ -1,4 +1,5 @@
 import type { LiveModelInt } from '../../../../../services/global/LiveModel/LiveModelInt';
+import type { EditorFloatMenuInt } from '../../../../../services/global/StringProvider/ModelEditorPageStrings/OtherStrings/EditorFloatMenu/EditorFloatMenuInt';
 import type { ModelEditorInt } from '../../../../../services/model-editor/ModelEditor/ModelEditorInt';
 
 export type VariableMenuButtonsProps = {
