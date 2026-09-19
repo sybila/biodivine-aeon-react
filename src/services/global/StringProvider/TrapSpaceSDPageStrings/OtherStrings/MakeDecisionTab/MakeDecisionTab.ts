4558a8decd004ef2d0547d852c1c5490e1c45d5d
MrMooshaber <vojtavesely1@seznam.cz>
2026-09-19 21:56:21 +0200

    fix: delete old MakeDecisionInt and MakeDecision files

--- src/services/global/StringProvider/TrapSpaceSDPageStrings/OtherStrings/MakeDecisionTab/MakeDecisionTab.ts
+++
@@ -1,9 +0,0 @@
-import type { MakeDecisionTabInt } from './MakeDecisionTabInt';
-
-class MakeDecisionTab implements MakeDecisionTabInt {
-  cannotMakeDecisionOnLeafNode(): string {
-    return 'Cannot make decision on minimal trap space (choose node which is not a leaf node to make further decisions).';
-  }
-}
-
-export default MakeDecisionTab;
--- src/services/global/StringProvider/TrapSpaceSDPageStrings/OtherStrings/MakeDecisionTab/MakeDecisionTabInt.ts
+++
@@ -1,4 +0,0 @@
-/** Class which provides general strings used in the make decision tab of Trap Space Succession Diagram Page. */
-export interface MakeDecisionTabInt {
-    cannotMakeDecisionOnLeafNode(): string;
-}
