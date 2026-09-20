b67376c054b5dfa3803a55150d80dc340f402771
MrMooshaber <vojtavesely1@seznam.cz>
2026-09-20 21:11:51 +0200

    fix: missing import

--- src/services/trap-space-succession-diagram/TrapSpaceSuccessionDiagram/TrapSpaceSuccessionDiagramInt.ts
+++ src/services/trap-space-succession-diagram/TrapSpaceSuccessionDiagram/TrapSpaceSuccessionDiagramInt.ts
@@ -1,4 +1,4 @@
-import type { NodeDataTSSDWithMotifs } from '../../../types/types';
+import type { DecisionTSSD, NodeDataTSSDWithMotifs } from '../../../types/types';
 
 /** Interface which defines service responsible for managing Trap Space Succession Diagram page.  */
 export interface TrapSpaceSuccessionDiagramInt {
